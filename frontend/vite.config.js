13dd3fa914900ee5408fbbbc6ef5aad2a040828b
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-07-25 02:36:21 +0530

    fix: update API target URL in Vite configuration for production deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 
   server: {
     "/api": {
-      target: "http://localhost:3000",
+      target: "https://lead-desk-mini-virid.vercel.app/",
       changeOrigin: true,
       secure: false,
       rewrite: (path) => path.replace(/^\/api/, ""),
