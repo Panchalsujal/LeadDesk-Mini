8c9e95e9e51e458d74d418304aa08149d73fc2d7
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-07-25 02:01:29 +0530

    feat: add MongoDB connection handling and Vercel configuration files

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,13 +4,14 @@ import {config} from "./config.js";
 
 // Connect to MongoDB
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
   try {
-    await mongoose.connect(config.MONGO_URI).then(() => {
-      console.log("Connected to MongoDB");
-    });
+    await mongoose.connect(config.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
   }
 };
 
